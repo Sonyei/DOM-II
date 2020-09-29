81e68d77a247f9ea1e72ffcf4826b8306797352e
Sonyei <esonyei@gmail.com>
2020-09-29 16:40:00 -0400

    3 done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+const firstLink = document.querySelector(".nav-link")
+console.log(firstLink)
+
+const xContainer = document.querySelector(".nav")
+console.log(xContainer)
+
+firstLink.addEventListener ('keydown', (event) => {
+if (event.key === 'a') {
+    xContainer.style.backgroundColor = "pink"
+    }
+})
+
+firstLink.addEventListener('mouseover', function(event)  {
+    event.target.style.color = "white"
+})
+
+firstLink.addEventListener('focus', function(event) {
+    event.target.style.color = "purple"
+})
